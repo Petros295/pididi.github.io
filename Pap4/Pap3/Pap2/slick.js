322823988d1da7a2ca792a6863f13be9608f4273
Petros295 <petrpetrosyan2005@gmail.com>
2024-10-31 19:57:43 +0300

    Add files via upload

---
+++ Pap4/Pap3/Pap2/slick.js
@@ -0,0 +1,23 @@
+﻿"use strict"
+$(document).ready(function () {
+	$(".gallery").slick({
+		dots: true,
+		infinite: true,
+		speed: 100,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		prevArrow: $('#prev'),
+		nextArrow: $('#next'),
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true
+				}
+			}
+		]
+	});
+});
